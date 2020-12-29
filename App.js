d3ddf17c6812746ed1a059e95c451634339c37df
Richard Valkinsenn <valkinsenn@gmail.com>
2020-12-29 17:12:51 -0600

    React Native Components

--- App.js
+++ App.js
@@ -1,21 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from "expo-status-bar";
+import React from "react";
+import Main from "./components/MainComponent";
+// import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Main />
+    // <View style={styles.container}>
+    //   <Text style={styles.baseText}>
+    //     Open up App.js to start working on your app!
+    //   </Text>
+    //   <Text style={styles.baseText}>What am I gunna DO?!!</Text>
+    //   <StatusBar style="auto" />
+    // </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#101010",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+//   baseText: {
+//     color: "#D0D0D0",
+//   },
+// });
