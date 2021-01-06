538673a2a18745c0d870ee137a87235bc1cb6610
Richard Valkinsenn <valkinsenn@gmail.com>
2021-01-05 19:59:50 -0600

    Icons, Favorites, and Comments

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,4 +1,10 @@
-import React from "react";
+// SUGGESTION 1, PART 1: You can import "Component" from the react library
+// No points off, but I noticed when defining your Main component as a class, you referred to React.Component.
+// That is absolutely fine, but I wanted to make sure you were also aware that you could import
+// Component in the line below like this...
+// OLD CODE: import React from "react";
+import React, { Component } from "react";
+// END SUGGESTION 1, PART 1
 import Home from "./HomeComponent";
 import Directory from "./DirectoryComponent";
 import About from "./AboutComponent";
@@ -13,13 +19,23 @@ import { createAppContainer } from "react-navigation";
 
 const ContactNavigator = createStackNavigator(
   {
+    // FIX 1, PART 1: Incorrect screen assignment
+    // Your contact navigator only needs to serve up the contact component
+    // You can delete the rest of the component pointers...
+    /*
     Directory: { screen: Directory },
     CampsiteInfo: { screen: CampsiteInfo },
     About: { screen: About },
+    */
+    // END FIX 1, PART 1
     Contact: { screen: Contact },
   },
   {
-    initialRouteName: "Contact",
+    // FIX 1, PART 2: Unnecessary initial route
+    // Since we now have corrected this navigator to point to one component,
+    // you do not need the "initialRoute" parameter.
+    /* initialRouteName: "Contact",*/
+    // END FIX 1, PART 2
     defaultNavigationOptions: {
       headerStyle: {
         backgroundColor: "#5637DD",
@@ -34,13 +50,22 @@ const ContactNavigator = createStackNavigator(
 
 const AboutNavigator = createStackNavigator(
   {
+    // FIX 2, PART 1: Incorrect screen assignment
+    // As above, your about navigator only needs to serve up the contact component
+    // You can delete the rest of the component pointers...
+    /*
     Directory: { screen: Directory },
     CampsiteInfo: { screen: CampsiteInfo },
-    About: { screen: About },
     Contact: { screen: Contact },
+    */
+    // END FIX 2, PART 1
+    About: { screen: About },
   },
   {
-    initialRouteName: "About",
+    // FIX 2, PART 2: Unnecessary initial route
+    // Again, if you have only one screen, there is no need to specify an initial route
+    /* initialRouteName: "About", */
+    // END FIX 2, PART 2
     defaultNavigationOptions: {
       headerStyle: {
         backgroundColor: "#5637DD",
@@ -57,8 +82,17 @@ const DirectoryNavigator = createStackNavigator(
   {
     Directory: { screen: Directory },
     CampsiteInfo: { screen: CampsiteInfo },
+    // FIX 3, PART 1: Incorrect screen assignment
+    // Your directory navigator still only needs to serve up the
+    // directory and campsite info components. Because we have more
+    // than one screen, we DO have to have the initial route paramter
+    // specified as you have it on line 98.
+    // You can delete the rest of the component pointers...
+    /*
     About: { screen: About },
     Contact: { screen: Contact },
+    */
+    // END FIX 3, PART 1
   },
   {
     initialRouteName: "Directory",
@@ -105,7 +139,12 @@ const MainNavigator = createDrawerNavigator(
 
 const AppNavigator = createAppContainer(MainNavigator);
 
-class Main extends React.Component {
+// SUGGESTION 1, PART 2: Using Component when imported from react library
+// As per the suggestion above, if you import Component from the react library
+// you would change your class definition statement just slightly...
+// OLD CODE: class Main extends React.Component {
+class Main extends Component {
+  // END SUGGESTION 1, PART 2
   // constructor(props) {
   //   super(props);
   //   this.state = {
